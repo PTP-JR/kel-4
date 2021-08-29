87234b171c39bb754e3e65308b2ff3dafc45d390
Sarahwp <sarahhandari.sh@gmail.com>
2021-08-29 19:39:23 +0700

    component data Harry Potter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Switch, Route, Link, Redirect } from "react-router-dom";
 import "./App.css";
 import DataHarryPotter from "./components/DataHarryPotter";
+import { useState } from "react";
+import Login from "./components/Login"
 
 function App() {
+
+  const [login, setLogin] = useState(true);
+
   return (
     <div className="App">
-      <>
+     
         <DataHarryPotter />
-      </>
+      
+       
+      
+      <Login />
     </div>
   );
 }
