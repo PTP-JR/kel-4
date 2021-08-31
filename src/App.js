8b5d7b6ed0fbe4a97db1d2ead866ebc1dbcec270
aditiyanurwahid <aditiyanurwahid1995@gmail.com>
2021-08-31 19:58:16 +0700

    rename

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import DataHarryPotter from "./components/DataHarryPotter";
-
-
 import Login from "./components/Login";
-
 import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
-
-      
-
       <Switch>
         <Route exact path="/">
           <Login />
@@ -24,7 +18,6 @@ function App() {
           <About />
         </Route>
       </Switch>
-
     </div>
   );
 }
